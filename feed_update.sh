789c4cb4f043c3c36bfa494136ff93890be23afa
madarasz <istvan.madarasz@xapo.com>
2023-12-06 09:27:43 +0100

    kill remaining firefoxes

--- feed_update.sh
+++ feed_update.sh
@@ -6,3 +6,4 @@ python3 scraper-wc-downloads.py
 cp feed-wc-killteam.xml /var/www/alwaysberunning/public/
 cp feed-gh-killteam.xml /var/www/alwaysberunning/public/
 cp feed-wc-dataslate.xml /var/www/alwaysberunning/public/
+pkill -f firefox-esr

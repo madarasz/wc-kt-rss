b4732a0d5e238ae16402d535634bddd33b92ff01
madarasz <istvan.madarasz@finpoints.com>
2024-09-30 15:42:36 +0200

    fix for new WH Community site

--- scraper-wc.py
+++ scraper-wc.py
@@ -2,24 +2,29 @@
 from scraper import Scraper
 
 # URL of the website to scrape
-url = 'https://www.warhammer-community.com/kill-team/'
+baseurl = 'https://www.warhammer-community.com'
+url = baseurl + '/en-gb/setting/kill-team/'
 
 
 scraper_instance = Scraper(headless=True, url=url)
-soup = scraper_instance.get_soup("screenshot-wc", "a.post-item")
+soup = scraper_instance.get_soup("screenshot-wc", "section.shared-newsGridThree")
 fg = scraper_instance.get_fg('Warhammer Community - Kill Team', 'Kill Team posts')
 
 # Extract news items
-news_items = soup.find_all('a', class_='post-item')
+news_items = soup.find_all('article')
 
 for item in news_items:
     title = item.find('h3').text.strip()
-    link = item['href']
-    description = item.find('div', class_='post-feed__excerpt').text.strip()
-    image_url = scraper_instance.image_url_from_css_background(item.find('div', class_='post-item__img-container'))
-    pub_date = scraper_instance.date_from_string(item.find('p', class_='post-item__date').get_text(strip=True))
+    link = item.find('a')['href']
+    image_url = ''
+    img_figure = item.find('figure')
+    if img_figure:
+        image_tag = img_figure.find('img')
+        if image_tag:
+            image_url = image_tag['src']  # Get the 'src' attribute for the image URL
+    pub_date = scraper_instance.date_from_string(item.find('time', class_='whitespace-nowrap').get_text(strip=True))
 
-    scraper_instance.add_item(title, link, description, image_url, pub_date)   
+    scraper_instance.add_item(title, baseurl + link, '', baseurl + image_url, pub_date)   
 
 # Save the RSS feed to a file
 fg.rss_file('feed-wc-killteam.xml')
